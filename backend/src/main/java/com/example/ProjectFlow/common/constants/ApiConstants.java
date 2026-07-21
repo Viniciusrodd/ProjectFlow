7347d007a5b3604160c6d8fab9f2e93b90480f88
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-07-21 14:26:48 -0300

    constants - apiConstants

---
+++ backend/src/main/java/com/example/ProjectFlow/common/constants/ApiConstants.java
@@ -0,0 +1,13 @@
+
+// packages
+package com.example.ProjectFlow.common.constants;
+
+
+public class ApiConstants {
+   
+   // base path
+   public static final String BASE_API_PREFIX = "/api";
+   public static final String API_VERSION = "/v1";
+   public static final String BASE_API_PATH = BASE_API_PREFIX + API_VERSION;
+
+}
