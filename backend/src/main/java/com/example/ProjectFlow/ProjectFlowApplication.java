13e39525c3619629e837b74b3c945716668e2dc2
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-07-21 09:22:32 -0300

    backend - initial struct build

---
+++ backend/src/main/java/com/example/ProjectFlow/ProjectFlowApplication.java
@@ -0,0 +1,18 @@
+
+// packages
+package com.example.ProjectFlow;
+
+// imports
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProjectFlowApplication {
+
+   // main
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFlowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/ProjectFlow/ProjectFlowApplicationTests.java
@@ -0,0 +1,17 @@
+
+// packages
+package com.example.ProjectFlow;
+
+// imports
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ProjectFlowApplicationTests {
+
+	@Test
+   	void contextLoads() {
+	}
+   
+}
