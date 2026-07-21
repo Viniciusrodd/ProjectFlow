64dc04457b09f116ef33aa39a214712fca894326
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-07-21 14:58:33 -0300

    constants - dateTimeConstants

---
+++ backend/src/main/java/com/example/ProjectFlow/common/constants/DateTimeConstants.java
@@ -0,0 +1,14 @@
+
+// packages
+package com.example.ProjectFlow.common.constants;
+
+
+public class DateTimeConstants {
+
+   // seconds
+   public static final long DAY_IN_SECONDS = 86400;
+   public static final long HOUR_IN_SECONDS = 3600;
+   public static final long WEEK_IN_SECONDS = 604800;
+   public static final long MONTH_IN_SECONDS = 2592000;
+
+}
