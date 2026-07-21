231c51a6663d4b2f601c285aa748bb59321ce834
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-07-21 14:34:05 -0300

    constants - errorConstants

---
+++ backend/src/main/java/com/example/ProjectFlow/common/constants/ErrorConstants.java
@@ -0,0 +1,17 @@
+
+// packages
+package com.example.ProjectFlow.common.constants;
+
+
+public class ErrorConstants {
+
+   // error messages
+   public static final String RESOURCE_NOT_FOUND = "Recurso não encontrado";
+   public static final String INVALID_CREDENTIALS = "Credenciais inválidas";
+   public static final String ACCESS_DENIED = "Acesso negado";
+   public static final String DUPLICATE_EMAIL = "E-mail já cadastrado";
+   public static final String USER_NOT_IN_ORGANIZATION = "Usuário não pertence à organização";
+   public static final String USER_NOT_IN_PROJECT = "Usuário não pertence ao projeto";
+   public static final String INVALID_ROLE = "Cargo de usuário inválido para esta operação";
+
+}
