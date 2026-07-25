f00336e146d39f5da9a1a8d4e2e11df3824ca35a
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-07-25 19:34:33 -0300

    project/entity - project members entity setup

--- backend/src/main/java/com/example/ProjectFlow/modules/project/entity/ProjectMembersEntity.java
+++ backend/src/main/java/com/example/ProjectFlow/modules/project/entity/ProjectMembersEntity.java
@@ -1,6 +1,6 @@
 
 // packages
-package com.example.ProjectFlow.modules.organization.entity;
+package com.example.ProjectFlow.modules.project.entity;
 
 // imports
 import java.time.LocalDateTime;
@@ -12,6 +12,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -19,38 +20,35 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
-import jakarta.persistence.FetchType;
 
 // import interfaces
 import com.example.ProjectFlow.common.interfaces.crudBase.SoftDelete;
 
 // import entities
-import com.example.ProjectFlow.modules.user.entity.UserEntity;
-
-// import enums
 import com.example.ProjectFlow.modules.organization.enums.RoleEnum;
+import com.example.ProjectFlow.modules.user.entity.UserEntity;
 
 
 @Entity
-@Table(name = "organization_members")
+@Table(name = "project_members")
 @SQLRestriction("deleted_at IS NULL")
-public class OrganizationMembersEntity implements SoftDelete {
+public class ProjectMembersEntity implements SoftDelete {
  
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
 
-   // N(users) : 1(organization_members)
+   // N(users) : 1(project_members)
    @ManyToOne(fetch = FetchType.LAZY)
    @JoinColumn(name = "user_id", nullable = false)
    private UserEntity user;
    
-   // N(organizations) : 1(organization_members)
+   // N(projects) : 1(project_members)
    @ManyToOne(fetch = FetchType.LAZY)
-   @JoinColumn(name = "organization_id", nullable = false)
-   private OrganizationEntity organization;
-   
-   
+   @JoinColumn(name = "project_id", nullable = false)
+   private ProjectEntity project;
+
+
    //// fields
    
 
@@ -71,13 +69,13 @@ public class OrganizationMembersEntity implements SoftDelete {
 
 
    // constructor
-   protected OrganizationMembersEntity() {} 
+   protected ProjectMembersEntity(){}
 
 
    // getters
    public Long getId() { return this.id; }
-   public OrganizationEntity getOrganization() { return this.organization; }
    public UserEntity getUser() { return this.user; }
+   public ProjectEntity getProject() { return this.project; }
    public RoleEnum getRole() { return this.role; }
    public LocalDateTime getJoinedAt() { return this.joinedAt; }
    public LocalDateTime getUpdatedAt() { return this.updatedAt; }
@@ -86,12 +84,12 @@ public class OrganizationMembersEntity implements SoftDelete {
 
    // setters
    public void setId(Long id) { this.id = id; }
-   public void setOrganization(OrganizationEntity organization) { this.organization = organization; }
    public void setUser(UserEntity user) { this.user = user; }
+   public void setProject(ProjectEntity project) { this.project = project; }
    public void setRole(RoleEnum role) { this.role = role; }
    public void setJoinedAt(LocalDateTime joinedAt) { this.joinedAt = joinedAt; }
    public void setUpdatedAt(LocalDateTime updatedAt) { this.updatedAt = updatedAt; }
-   public void setDeletedAt(LocalDateTime deletedAt) { this.deletedAt = deletedAt; }
+   public void setDeletedAt(LocalDateTime deletedAt) { this.deletedAt = deletedAt; } 
 
 
    // utils
