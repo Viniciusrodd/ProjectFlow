7dfcdb6d7cf7b2405bb63d58f94ed293dd3f6bf0
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-07-25 20:54:37 -0300

    board/entity - board entity setup

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/board/entity/BoardEntity.java
@@ -0,0 +1,84 @@
+
+// packages
+package com.example.ProjectFlow.modules.board.entity;
+
+// imports
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+// jakarta imports
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+// import interfaces
+import com.example.ProjectFlow.common.interfaces.crudBase.SoftDelete;
+
+// import entities
+import com.example.ProjectFlow.modules.project.entity.ProjectEntity;
+
+
+@Entity
+@Table(name = "board")
+@SQLRestriction("deleted_at IS NULL")
+public class BoardEntity implements SoftDelete {
+ 
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   // 1(project) : 1(board)
+   @OneToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "project_id", nullable = false)
+   private ProjectEntity project;
+
+
+   //// fields
+
+
+   @Column(nullable = false, length = 120)
+   private String name;
+
+   @CreationTimestamp
+   @Column(name = "created_at", updatable = false)
+   private LocalDateTime createdAt;
+
+   @UpdateTimestamp
+   @Column(name = "updated_at")
+   private LocalDateTime updatedAt;
+
+   @Column(name = "deleted_at")
+   private LocalDateTime deletedAt;
+
+
+   // getters
+   public Long getId() { return this.id; }
+   public ProjectEntity getProject() { return this.project; }
+   public String getName() { return this.name; }
+   public LocalDateTime getCreatedAt() { return this.createdAt; }
+   public LocalDateTime getUpdatedAt() { return this.updatedAt; }
+   public LocalDateTime getDeletedAt() { return this.deletedAt; }
+
+
+   // setters
+   public void setId(Long id) { this.id = id; }
+   public void setProject(ProjectEntity project) { this.project = project; }
+   public void setName(String name) { this.name = name; }
+   public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+   public void setUpdatedAt(LocalDateTime updatedAt) { this.updatedAt = updatedAt; }
+   public void setDeletedAt(LocalDateTime deletedAt) { this.deletedAt = deletedAt; }
+
+
+   // utils
+   public boolean isDeleted() { return this.deletedAt != null; }
+
+}
