cce96fd2e00e509206ffa20e9e89b2f5d99afd7d
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-07-26 21:29:10 -0300

    comment/entity - comment entity setup

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/comment/entity/CommentEntity.java
@@ -0,0 +1,95 @@
+
+// packages
+package com.example.ProjectFlow.modules.comment.entity;
+
+// imports
+import java.time.LocalDateTime;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+// jakarta imports
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+// import interfaces
+import com.example.ProjectFlow.common.interfaces.crudBase.SoftDelete;
+
+// import entities
+import com.example.ProjectFlow.modules.user.entity.UserEntity;
+import com.example.ProjectFlow.modules.task.entity.TasksEntity;
+
+
+@Entity
+@Table(name = "comments")
+@SQLRestriction("deleted_at IS NULL")
+public class CommentEntity implements SoftDelete {
+   
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   // N(comments) : 1(task)
+   @ManyToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "task_id", nullable = false)
+   private TasksEntity task;
+
+   // N(comments) : 1(author user)
+   @ManyToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "author_id", nullable = false)
+   private UserEntity author;
+
+
+   //// fields 
+
+
+   @Column(length = 500, nullable = false)
+   private String content;
+
+   @CreationTimestamp
+   @Column(name = "created_at", updatable = false)
+   private LocalDateTime createdAt;
+
+   @UpdateTimestamp
+   @Column(name = "updated_at")
+   private LocalDateTime updatedAt;
+
+   @Column(name = "deleted_at")
+   private LocalDateTime deletedAt;
+
+
+   // constructor
+   protected CommentEntity() {}
+
+
+   // getters
+   public Long getId() { return this.id; }
+   public TasksEntity getTask() { return this.task; }
+   public UserEntity getAuthor() { return this.author; }
+   public String getContent() { return this.content; }
+   public LocalDateTime getCreatedAt() { return this.createdAt; }
+   public LocalDateTime getUpdatedAt() { return this.updatedAt; }
+   public LocalDateTime getDeletedAt() { return this.deletedAt; }
+
+
+   // setters
+   public void setId(Long id) { this.id = id; }
+   public void setTask(TasksEntity task) { this.task = task; }
+   public void setAuthor(UserEntity author) { this.author = author; }      
+   public void setContent(String content) { this.content = content; }
+   public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+   public void setUpdatedAt(LocalDateTime updatedAt) { this.updatedAt = updatedAt; }
+   public void setDeletedAt(LocalDateTime deletedAt) { this.deletedAt = deletedAt; }
+
+
+   // utils
+   public boolean isDeleted() { return this.deletedAt != null; }
+
+}
