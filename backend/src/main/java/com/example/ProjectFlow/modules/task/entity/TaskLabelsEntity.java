c9ea8a1fde7b4f1e3a9504383ca8753584ba9b65
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-07-28 17:29:53 -0300

    task/entity - task_labels entity setup

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/task/entity/TaskLabelsEntity.java
@@ -0,0 +1,83 @@
+
+// packages
+package com.example.ProjectFlow.modules.task.entity;
+
+// imports
+import java.time.LocalDateTime;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.CreationTimestamp;
+
+// jakarta imports
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+// import interfaces
+import com.example.ProjectFlow.common.interfaces.crudBase.SoftDelete;
+
+
+@Entity
+@Table(
+   name = "task_labels", 
+   uniqueConstraints = { @UniqueConstraint(columnNames = { "task_id", "label_id" }) } // (task + label id) combination must be unique
+)
+@SQLRestriction("deleted_at IS NULL")
+public class TaskLabelsEntity implements SoftDelete {
+ 
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   // N(tasks) : 1(task_labels)
+   @ManyToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "task_id", nullable = false)
+   private TasksEntity task;
+
+   // N(labels) : 1(task_labels)
+   @ManyToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "label_id", nullable = false)
+   private LabelsEntity label;
+
+
+   //// fields
+   
+
+   @CreationTimestamp
+   @Column(name = "created_at", updatable = false)
+   private LocalDateTime createdAt;
+
+   @Column(name = "deleted_at")
+   private LocalDateTime deletedAt;
+
+
+   // constructor
+   protected TaskLabelsEntity() {}
+
+
+   // getters
+   public Long getId() { return this.id; }
+   public TasksEntity getTask() { return this.task; }
+   public LabelsEntity getLabel() { return this.label; }
+   public LocalDateTime getCreatedAt() { return this.createdAt; }
+   public LocalDateTime getDeletedAt() { return this.deletedAt; }
+
+
+   // setters
+   public void setId(Long id) { this.id = id; }
+   public void setTask(TasksEntity task) { this.task = task; }
+   public void setLabel(LabelsEntity label) { this.label = label; }
+   public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+   public void setDeletedAt(LocalDateTime deletedAt) { this.deletedAt = deletedAt; }
+   
+
+   // utils
+   public boolean isDeleted() { return this.deletedAt != null; }
+
+}
