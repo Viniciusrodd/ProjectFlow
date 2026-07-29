e5357a827bb8c4c1552e9488fe2bdd26e81dca05
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-07-28 21:02:12 -0300

    project/documents - project images document - setup

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/project/document/ProjectImagesDocument.java
@@ -0,0 +1,62 @@
+
+// packages
+package com.example.ProjectFlow.modules.project.document;
+
+// imports
+import java.sql.Date;
+import org.springframework.data.annotation.Id;
+
+// mongodb imports
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+
+@Document(collection = "project_images")
+public class ProjectImagesDocument {
+ 
+   @Id
+   private String id;
+
+   @Field("projectId")
+   private Long projectId;
+
+   @Field("fileName")
+   private String fileName;
+
+   @Field("mimeType")
+   private String mimeType;
+
+   @Field("size")
+   private Long size;
+
+   @Field("uploadDate")
+   private Date uploadDate;
+   
+   @Field("binary")
+   private byte[] binary; // image data
+
+
+   // constructor
+   protected ProjectImagesDocument() {}
+
+
+   // getters
+   public String getId() { return this.id; }
+   public Long getProjectId() { return this.projectId; }
+   public String getFileName() { return this.fileName; }
+   public String getMimeType() { return this.mimeType; }
+   public Long getSize() { return this.size; }
+   public Date getUploadDate() { return this.uploadDate; }
+   public byte[] getBinary() { return this.binary; }
+
+
+   // setters
+   public void setId(String id) { this.id = id; }
+   public void setProjectId(Long projectId) { this.projectId = projectId; }
+   public void setFileName(String fileName) { this.fileName = fileName; }
+   public void setMimeType(String mimeType) { this.mimeType = mimeType; }
+   public void setSize(Long size) { this.size = size; }
+   public void setUploadDate(Date uploadDate) { this.uploadDate = uploadDate; }
+   public void setBinary(byte[] binary) { this.binary = binary; }
+
+}
