4dc072705f4100834c1d01afd04085b3b3f6ee4e
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-07-28 21:17:23 -0300

    activityLog/documents - activityLog document - setup

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/activityLog/document/ActivityLogDocument.java
@@ -0,0 +1,67 @@
+
+// packages
+package com.example.ProjectFlow.modules.activityLog.document;
+
+// imports
+import java.sql.Date;
+import org.springframework.data.annotation.Id;
+
+// mongodb imports
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+
+@Document(collection = "activity_logs")
+public class ActivityLogDocument {
+ 
+   @Id
+   private String id;
+
+   @Field("organizationId")
+   private Long organizationId; // mysql ref.
+
+   @Field("projectId")
+   private Long projectId; // mysql ref.
+
+   @Field("taskId")
+   private Long taskId; // mysql ref.
+
+   @Field("userId")
+   private Long userId; // mysql ref.
+
+   @Field("action")
+   private String action;
+
+   @Field("description")
+   private String description;
+
+   @Field("createdAt")
+   private Date createdAt;
+
+
+   // constructor
+   protected ActivityLogDocument() {}
+
+   
+   // getters
+   public String getId() { return this.id; }
+   public Long getOrganizationId() { return this.organizationId; }
+   public Long getProjectId() { return this.projectId; }
+   public Long getTaskId() { return this.taskId; }
+   public Long getUserId() { return this.userId; }
+   public String getAction() { return this.action; }
+   public String getDescription() { return this.description; }
+   public Date getCreatedAt() { return this.createdAt; }
+
+
+   // setters
+   public void setId(String id) { this.id = id; }
+   public void setOrganizationId(Long organizationId) { this.organizationId = organizationId; }
+   public void setProjectId(Long projectId) { this.projectId = projectId; }
+   public void setTaskId(Long taskId) { this.taskId = taskId; }
+   public void setUserId(Long userId) { this.userId = userId; }
+   public void setAction(String action) { this.action = action; }
+   public void setDescription(String description) { this.description = description; }
+   public void setCreatedAt(Date createdAt) { this.createdAt = createdAt; }
+
+}
