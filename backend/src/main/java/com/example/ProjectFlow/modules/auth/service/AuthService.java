ba75c743fae2dd4a4e87b2c44073c5efa373c98f
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-07-31 17:17:30 -0300

    auth/service - authService register - setup

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/auth/service/AuthService.java
@@ -0,0 +1,67 @@
+
+// packages
+package com.example.ProjectFlow.modules.auth.service;
+
+// imports
+import org.springframework.stereotype.Service;
+
+// import repository
+import com.example.ProjectFlow.modules.auth.repository.AuthRepository;
+
+// import validators
+import com.example.ProjectFlow.modules.auth.validator.RegisterValidator;
+import com.example.ProjectFlow.modules.auth.validator.LoginValidator;
+
+// import security
+import com.example.ProjectFlow.security.JWT.JwtService;
+
+// import DTOs
+import com.example.ProjectFlow.modules.auth.dto.RegisterDTO;
+import com.example.ProjectFlow.modules.auth.dto.RegisterResponseDTO;
+
+
+@Service
+public class AuthService {
+ 
+   // properties
+   private final AuthRepository authRepository;
+   private final PasswordService passwordService;
+   private final RegisterValidator registerValidator;
+   private final LoginValidator loginValidator;
+   private final JwtService jwtService;
+
+   // constructor - dependency injection
+   public AuthService(
+      AuthRepository authRepository,
+      PasswordService passwordService,
+      RegisterValidator registerValidator,
+      LoginValidator loginValidator,
+      JwtService jwtService
+   ) {
+      this.authRepository = authRepository;
+      this.passwordService = passwordService;
+      this.registerValidator = registerValidator;
+      this.loginValidator = loginValidator;
+      this.jwtService = jwtService;
+   }
+
+
+   // register
+   public RegisterResponseDTO register(RegisterDTO data) {
+      // validation
+      this.registerValidator.validate(data);
+
+      // encrypt password
+      final String encryptedPassword = this.passwordService.encryptPassword(data.password());
+
+      // user data - setup
+      RegisterDTO userData = new RegisterDTO(
+         data.name(),
+         data.email(),
+         encryptedPassword
+      );
+
+      return this.authRepository.register(userData);
+   }
+
+}
