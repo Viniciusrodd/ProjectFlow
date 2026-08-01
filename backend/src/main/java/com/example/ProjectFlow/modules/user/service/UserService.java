9848a1e65f4573dd8a7a771fd67821f03e079127
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-01 14:35:45 -0300

    user/service - userService - find by email

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/user/service/UserService.java
@@ -0,0 +1,58 @@
+
+// packages
+package com.example.ProjectFlow.modules.user.service;
+
+// imports
+import org.springframework.stereotype.Service;
+
+import jakarta.persistence.NoResultException;
+import jakarta.transaction.Transactional;
+
+// import repository
+import com.example.ProjectFlow.modules.user.repository.UserRepository;
+
+// import validator
+import com.example.ProjectFlow.modules.user.validator.UserValidator;
+
+// import DTOs
+import com.example.ProjectFlow.modules.user.dto.UserDTO;
+
+// import exceptions
+import com.example.ProjectFlow.exception.MultiExceptions;
+
+// import constants
+import com.example.ProjectFlow.common.constants.ResponseMessages;
+
+
+@Service
+public class UserService {
+
+   // properties
+   private final UserRepository userRepository;
+   private final UserValidator userValidator;
+
+   // constructor - dependency injection
+   public UserService(
+      UserRepository userRepository,
+      UserValidator userValidator
+   ) {
+      this.userRepository = userRepository;
+      this.userValidator = userValidator;
+   }
+
+
+   // find by email
+   @Transactional
+   public UserDTO findByEmail(String email) {
+      // validation
+      this.userValidator.emailValidate(email);
+
+      try {
+         return this.userRepository.findByEmail(email);
+      }
+      catch (NoResultException error) {
+         throw MultiExceptions.notFound(ResponseMessages.NOT_FOUND);
+      }
+   }
+
+}
