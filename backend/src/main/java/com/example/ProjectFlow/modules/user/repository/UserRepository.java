9bee43e361e24230773907c15dd0022375659eaa
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-01 14:04:48 -0300

    user/repository - user repository - find by email

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/user/repository/UserRepository.java
@@ -0,0 +1,40 @@
+
+// packages
+package com.example.ProjectFlow.modules.user.repository;
+
+// imports
+import org.springframework.stereotype.Repository;
+
+// jakarta imports
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.EntityManager;
+
+// import DTOs
+import com.example.ProjectFlow.modules.user.dto.UserDTO;
+
+// import entity
+import com.example.ProjectFlow.modules.user.entity.UserEntity;
+
+
+@Repository
+public class UserRepository {
+ 
+   // properties
+   @PersistenceContext
+   private EntityManager entityManager;
+
+   // find by email
+   public UserDTO findByEmail(String email) {
+      UserEntity user = entityManager
+         .createQuery("SELECT u FROM UserEntity u WHERE u.email = :email", UserEntity.class)
+         .setParameter("email", email)
+         .getSingleResult();
+      
+      return new UserDTO(
+         user.getId(),
+         user.getEmail(),
+         user.getName()
+      );
+   }
+
+}
