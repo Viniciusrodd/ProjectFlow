d31622d178b6438cf3b2058b52cfbe37073e008a
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-02 16:59:24 -0300

    user/controller - profile image controller - implementation

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/user/controller/ProfileImageController.java
@@ -0,0 +1,63 @@
+
+// packages
+package com.example.ProjectFlow.modules.user.controller;
+
+// imports
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+// import constants
+import com.example.ProjectFlow.common.constants.ApiConstants;
+
+// import services
+import com.example.ProjectFlow.modules.user.service.ProfileImageService;
+
+// import responses
+import com.example.ProjectFlow.common.responses.ApiResponse;
+
+// import DTOs
+import com.example.ProjectFlow.modules.user.dto.ProfileImageResponseDTO;
+
+// import constants
+import com.example.ProjectFlow.common.constants.ResponseMessages;
+
+
+@RestController
+@RequestMapping(ApiConstants.BASE_API_PATH + "/users/profile-image")
+public class ProfileImageController {
+ 
+   // properties
+   private ProfileImageService profileImageService;
+
+   // constructor - dependency injection
+   public ProfileImageController(ProfileImageService profileImageService) {
+      this.profileImageService = profileImageService;
+   }
+
+
+   // upload profile image
+   @PostMapping(value = "/{userId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+   public ResponseEntity<ApiResponse<ProfileImageResponseDTO>> uploadProfileImage(
+      @PathVariable Long userId, 
+      @RequestParam("file") MultipartFile file
+   ) {
+      ProfileImageResponseDTO profileImageData = this.profileImageService.uploadProfileImage(userId, file);
+
+      ApiResponse<ProfileImageResponseDTO> response = new ApiResponse.Builder<ProfileImageResponseDTO>()
+         .success(true)
+         .statusCode(HttpStatus.CREATED.value())
+         .message(ResponseMessages.CREATED)
+         .data(profileImageData)
+         .build();
+      
+      return ResponseEntity.status(HttpStatus.CREATED).body(response);
+   }
+
+}
