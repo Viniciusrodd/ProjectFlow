233604c8fee56146c62970f4a5c0c106c1e3e68a
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-05 11:06:37 -0300

    user/validator - update validation - setup

--- backend/src/main/java/com/example/ProjectFlow/modules/user/validator/UserValidator.java
+++ backend/src/main/java/com/example/ProjectFlow/modules/user/validator/UserValidator.java
@@ -12,9 +12,16 @@ import com.example.ProjectFlow.exception.MultiExceptions;
 import com.example.ProjectFlow.common.constants.ResponseMessages;
 import com.example.ProjectFlow.common.constants.ValidationConstants;
 
+// import DTO
+import com.example.ProjectFlow.modules.user.dto.UserUpdateDTO;
+
 
 @Component
 public class UserValidator {
+
+   // properties
+   String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+
  
    // id validate
    public void idValidate(Long id) {
@@ -43,8 +50,7 @@ public class UserValidator {
          ));
       }
 
-      String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
-      if(!email.matches(emailRegex)) {
+      if(!email.matches(this.emailRegex)) {
          throw MultiExceptions.invalid(String.format(
             "%s: Formato de email inválido",
             ResponseMessages.INVALID_DATA
@@ -58,6 +64,73 @@ public class UserValidator {
             ValidationConstants.MAX_EMAIL_LENGTH
          ));
       }
+   }
+
+
+   // update validations
+   public void updateValidations(UserUpdateDTO data) {
+      // name
+      data.name().ifPresent(name -> {
+         if(name.trim().isEmpty()) {
+            throw MultiExceptions.badRequest(String.format(
+               "%s: Nome não pode ser vazio",
+               ResponseMessages.BAD_REQUEST
+            ));
+         }
+
+         if(name.length() < ValidationConstants.MIN_NAME_LENGTH || name.length() > ValidationConstants.MAX_NAME_LENGTH) {
+            throw MultiExceptions.invalid(String.format(
+               "%s: Nome deve estar entre %d e %d caracteres",
+               ResponseMessages.INVALID_DATA,
+               ValidationConstants.MIN_NAME_LENGTH,
+               ValidationConstants.MAX_NAME_LENGTH
+            ));
+         }
+      });
+
+      // email
+      data.email().ifPresent(email -> {
+         if(email.trim().isEmpty()) {
+            throw MultiExceptions.badRequest(String.format(
+               "%s: Email não pode ser vazio",
+               ResponseMessages.BAD_REQUEST
+            ));
+         }
+
+         if(!email.matches(this.emailRegex)) {
+            throw MultiExceptions.invalid(String.format(
+               "%s: Formato de email inválido",
+               ResponseMessages.INVALID_DATA
+            ));
+         }
+
+         if(email.length() > ValidationConstants.MAX_EMAIL_LENGTH) {
+            throw MultiExceptions.invalid(String.format(
+               "%s: Email deve ser menor que %d caracteres",
+               ResponseMessages.INVALID_DATA,
+               ValidationConstants.MAX_EMAIL_LENGTH
+            ));
+         }
+      });
+
+      // password
+      data.password().ifPresent(password -> {
+         if(password.trim().isEmpty()) {
+            throw MultiExceptions.badRequest(String.format(
+               "%s: Senha não pode ser vazia",
+               ResponseMessages.BAD_REQUEST
+            ));
+         }
+
+         if(password.length() < ValidationConstants.MIN_PASSWORD_LENGTH || password.length() > ValidationConstants.MAX_PASSWORD_LENGTH) {
+            throw MultiExceptions.invalid(String.format(
+               "%s: Senha deve estar entre %d e %d caracteres",
+               ResponseMessages.INVALID_DATA,
+               ValidationConstants.MIN_PASSWORD_LENGTH,
+               ValidationConstants.MAX_PASSWORD_LENGTH
+            ));
+         }
+      });
    }
 
 }
