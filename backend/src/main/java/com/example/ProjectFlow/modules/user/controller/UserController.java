623bc0e8e1b7c8f6869e7d9c100ca717a22c5aec
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-05 13:57:15 -0300

    user/controller - user update - setup

--- backend/src/main/java/com/example/ProjectFlow/modules/user/controller/UserController.java
+++ backend/src/main/java/com/example/ProjectFlow/modules/user/controller/UserController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 // http imports
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 // swagger imports
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 // import constants
 import com.example.ProjectFlow.common.constants.ApiConstants;
@@ -28,6 +30,7 @@ import com.example.ProjectFlow.common.responses.ApiResponse;
 
 // import DTOs
 import com.example.ProjectFlow.modules.user.dto.UserProfileDTO;
+import com.example.ProjectFlow.modules.user.dto.UserUpdateDTO;
 
 // import constants
 import com.example.ProjectFlow.common.constants.ResponseMessages;
@@ -73,6 +76,30 @@ public class UserController {
          .success(true)
          .statusCode(HttpStatus.OK.value())
          .message(ResponseMessages.FOUND)
+         .data(user)
+         .build();
+
+      return ResponseEntity.status(HttpStatus.OK).body(response);
+   }
+
+
+   // update user
+   @PutMapping(value = "/user/{userId}")
+   @Operation(summary = "Update user")
+   public ResponseEntity<ApiResponse<UserProfileDTO>> updateUser(
+      @PathVariable Long userId, 
+      @RequestBody UserUpdateDTO data
+   ) {
+      System.out.println("------------------------------------------");
+      System.out.println(data);
+      System.out.println("------------------------------------------");
+
+      UserProfileDTO user = this.userService.updateUser(userId, data);
+
+      ApiResponse<UserProfileDTO> response = new ApiResponse.Builder<UserProfileDTO>()
+         .success(true)
+         .statusCode(HttpStatus.OK.value())
+         .message(ResponseMessages.UPDATED)
          .data(user)
          .build();
 
