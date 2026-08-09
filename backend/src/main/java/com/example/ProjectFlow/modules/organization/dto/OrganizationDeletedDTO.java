ede6b5681258ac677db7e1a3cfcf64e7bb085de8
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-09 15:54:40 -0300

    organization/dto - delete organization dto

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/organization/dto/OrganizationDeletedDTO.java
@@ -0,0 +1,33 @@
+
+// packages
+package com.example.ProjectFlow.modules.organization.dto;
+
+import java.time.LocalDateTime;
+// imports
+import java.util.UUID;
+
+// import entity
+import com.example.ProjectFlow.modules.organization.entity.OrganizationEntity;
+
+
+public record OrganizationDeletedDTO (
+
+   UUID id,
+   UUID ownerId,
+   String name,
+   String description,
+   LocalDateTime deletedAt
+
+) {
+
+   public static OrganizationDeletedDTO get(OrganizationEntity document) {
+      return new OrganizationDeletedDTO(
+         document.getId(),
+         document.getOwnerId(),
+         document.getName(),
+         document.getDescription(),
+         document.getDeletedAt()
+      );
+   }
+
+}
