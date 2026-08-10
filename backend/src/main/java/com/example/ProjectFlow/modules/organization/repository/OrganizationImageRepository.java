bffddbf8879cb1d530843ba6252ee7e945819114
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-10 15:04:44 -0300

    organization/repository - organization image repository - setup

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/organization/repository/OrganizationImageRepository.java
@@ -0,0 +1,26 @@
+
+// packages
+package com.example.ProjectFlow.modules.organization.repository;
+
+// imports
+import java.util.UUID;
+
+// mongo imports
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+// import document
+import com.example.ProjectFlow.modules.organization.document.OrganizationImageDocument;
+
+
+public interface OrganizationImageRepository extends MongoRepository<OrganizationImageDocument, String> {
+ 
+   // find image by organization id
+   OrganizationImageDocument findByOrganizationId(UUID organizationId);
+
+   // delete image by organization id
+   void deleteByOrganizationId(UUID organizationId);
+
+   // exists by organization id
+   OrganizationImageDocument existsByOrganizationId(UUID organizationId);
+
+}
--- backend/src/main/java/com/example/ProjectFlow/modules/user/repository/ProfileImageRepository.java
+++ backend/src/main/java/com/example/ProjectFlow/modules/user/repository/ProfileImageRepository.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 // mongo imports
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-// import entities
+// import document
 import com.example.ProjectFlow.modules.user.document.ProfileImageDocument;
 
 
