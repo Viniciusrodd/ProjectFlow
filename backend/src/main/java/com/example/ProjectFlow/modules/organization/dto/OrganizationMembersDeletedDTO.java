965d0f83cde16400ff882c9938b7843889d3d9df
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-13 14:00:40 -0300

    organization/dto - organization members deleted DTO - setup

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/organization/dto/OrganizationMembersDeletedDTO.java
@@ -0,0 +1,36 @@
+
+// packages
+package com.example.ProjectFlow.modules.organization.dto;
+
+// imports
+import java.util.UUID;
+import java.time.LocalDateTime;
+
+// import enum
+import com.example.ProjectFlow.modules.organization.enums.RoleEnum;
+
+// import entity
+import com.example.ProjectFlow.modules.organization.entity.OrganizationMembersEntity;
+
+
+public record OrganizationMembersDeletedDTO (
+
+   UUID organizationId,
+   UUID userId,
+   RoleEnum role,
+   LocalDateTime joinedAt,
+   LocalDateTime deletedAt
+
+) {
+
+   public static OrganizationMembersDeletedDTO get(OrganizationMembersEntity document) {
+      return new OrganizationMembersDeletedDTO(
+         document.getOrganizationId(),
+         document.getUserId(),
+         document.getRole(),
+         document.getJoinedAt(),
+         document.getDeletedAt()
+      );
+   }
+
+}
