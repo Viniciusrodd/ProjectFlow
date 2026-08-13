ecb6e66eec20040d1c7b1709a323c9a7f1c46aa8
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-13 18:22:24 -0300

    project/dto - project response dto - changes

--- backend/src/main/java/com/example/ProjectFlow/modules/project/dto/ProjectResponseDTO.java
+++ backend/src/main/java/com/example/ProjectFlow/modules/project/dto/ProjectResponseDTO.java
@@ -14,6 +14,7 @@ import com.example.ProjectFlow.modules.project.entity.ProjectEntity;
 
 public record ProjectResponseDTO (
 
+   UUID id,
    UUID organizationId,
    UUID ownerId,
    String name,
@@ -24,6 +25,7 @@ public record ProjectResponseDTO (
 
    public static ProjectResponseDTO get(ProjectEntity document) {
       return new ProjectResponseDTO(
+         document.getId(),
          document.getOrganizationId(),
          document.getOwnerId(),
          document.getName(),
