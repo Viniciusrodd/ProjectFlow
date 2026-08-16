3793e71cb712c8d586f8c8811e960cc715bf064a
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-16 16:37:05 -0300

    project/service - project image service - get all project images

--- backend/src/main/java/com/example/ProjectFlow/modules/project/service/ProjectImageService.java
+++ backend/src/main/java/com/example/ProjectFlow/modules/project/service/ProjectImageService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 // jakarta imports
@@ -100,12 +101,26 @@ public class ProjectImageService {
       ProjectImageDocument image = this.projectImageRepository.findByProjectId(projectId);
       if(image == null) {
          throw MultiExceptions.notFound(String.format(
-            "%s: Imagem de organização não existe",
+            "%s: Imagem do projeto não existe",
             ResponseMessages.NOT_FOUND
          ));
       }
 
       return image;
+   }
+
+
+   // get all project images
+   public List<ProjectImageDocument> getAllProjectImages() {
+      List<ProjectImageDocument> images = this.projectImageRepository.findAll();
+      if(images.isEmpty()) {
+         throw MultiExceptions.notFound(String.format(
+            "%s: Imagens de projetos não existem",
+            ResponseMessages.NOT_FOUND
+         ));
+      }
+
+      return images;
    }
 
 }
