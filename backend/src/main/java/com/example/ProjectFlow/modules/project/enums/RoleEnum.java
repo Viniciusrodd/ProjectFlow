15aa1e915a988696eef1a69c1f4de0058817a63f
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-16 18:45:22 -0300

    project/enums - project role enums - setup

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/project/enums/RoleEnum.java
@@ -0,0 +1,34 @@
+
+// packages
+package com.example.ProjectFlow.modules.project.enums;
+
+
+public enum RoleEnum {
+ 
+   ADMIN("admin"),
+   MEMBER("member");
+
+   private final String type;
+
+   // constructor
+   private RoleEnum(String type) {
+      this.type = type;
+   }
+
+   // getters
+   public String getType() { return type; }
+
+   // is valid
+   public static boolean isValid(String type) {
+      if(type == null) return false;
+      
+      for(RoleEnum role : RoleEnum.values()) {
+         if(role.getType().equalsIgnoreCase(type)) {
+            return true;
+         }
+      }
+      
+      return false;
+   }
+
+}
