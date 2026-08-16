77ad6bdd5da89d4af5307177029ff90ed75a09d4
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-16 15:32:03 -0300

    project/controller - project image controller - upload image

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/project/controller/ProjectImageController.java
@@ -0,0 +1,72 @@
+
+// packages
+package com.example.ProjectFlow.modules.project.controller;
+
+// imports
+import java.util.UUID;
+
+// web imports
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+// http imports
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+// swagger imports
+import io.swagger.v3.oas.annotations.Operation;
+
+// import constants
+import com.example.ProjectFlow.common.constants.ApiConstants;
+
+// import services
+import com.example.ProjectFlow.modules.project.service.ProjectImageService;
+
+// import responses
+import com.example.ProjectFlow.common.responses.ApiResponse;
+
+// import DTOs
+import com.example.ProjectFlow.modules.project.dto.ProjectImageResponseDTO;
+
+// import constants
+import com.example.ProjectFlow.common.constants.ResponseMessages;
+
+
+@RestController
+@RequestMapping(ApiConstants.BASE_API_PATH + "/project/logo-image")
+public class ProjectImageController {
+ 
+   // properties
+   private final ProjectImageService projectImageService;
+   
+   // constructor - dependency injection
+   public ProjectImageController(ProjectImageService projectImageService) {
+      this.projectImageService = projectImageService;
+   }
+
+
+   // upload project image
+   @PostMapping(value = "/{projectId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+   @Operation(summary = "Upload project image")
+   public ResponseEntity<ApiResponse<ProjectImageResponseDTO>> uploadProjectImage(
+      @PathVariable UUID projectId,
+      @RequestParam MultipartFile file
+   ) {
+      ProjectImageResponseDTO projectImageData = this.projectImageService.uploadProjectImage(projectId, file);
+
+      ApiResponse<ProjectImageResponseDTO> response = new ApiResponse.Builder<ProjectImageResponseDTO>()
+         .success(true)
+         .statusCode(HttpStatus.CREATED.value())
+         .message(ResponseMessages.CREATED)
+         .data(projectImageData)
+         .build();
+      
+      return ResponseEntity.status(HttpStatus.CREATED).body(response);
+   }
+
+}
