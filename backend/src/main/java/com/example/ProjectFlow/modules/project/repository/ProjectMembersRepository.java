910f808417489399afad3a3bf9ba5d6d53ab19a9
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-17 17:41:58 -0300

    project/repository - project members repository - get all project members

--- backend/src/main/java/com/example/ProjectFlow/modules/project/repository/ProjectMembersRepository.java
+++ backend/src/main/java/com/example/ProjectFlow/modules/project/repository/ProjectMembersRepository.java
@@ -5,6 +5,9 @@ package com.example.ProjectFlow.modules.project.repository;
 // imports
 import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 // jakarta imports
 import jakarta.persistence.PersistenceContext;
@@ -13,6 +16,7 @@ import jakarta.persistence.EntityManager;
 
 // import DTOs
 import com.example.ProjectFlow.modules.project.dto.ProjectMembersResponseDTO;
+import com.example.ProjectFlow.modules.project.dto.ProjectMembersCompleteResponseDTO;
 import com.example.ProjectFlow.modules.project.dto.ProjectMembersDTO;
 
 // import entity
@@ -49,6 +53,30 @@ public class ProjectMembersRepository {
       this.entityManager.persist(projectMembers);
 
       return ProjectMembersResponseDTO.get(projectMembers);
+   }
+
+
+   // get all project members
+   public List<ProjectMembersCompleteResponseDTO> getAllMembersByProjectId(UUID projectId) {
+      List<ProjectMembersEntity> members = this.entityManager
+         .createQuery(
+            "SELECT m FROM ProjectMembersEntity m " +
+            "JOIN FETCH m.user " +
+            "JOIN FETCH m.project " +
+            "WHERE m.project.id = :projectId " +
+            "ORDER BY m.joinedAt DESC",
+            ProjectMembersEntity.class
+         )
+         .setParameter("projectId", projectId)
+         .getResultList();
+
+         List<ProjectMembersCompleteResponseDTO> allMembers = new ArrayList<>();
+
+         for(ProjectMembersEntity member : members) {
+            allMembers.add(ProjectMembersCompleteResponseDTO.get(member));
+         }
+
+         return allMembers;
    }
 
 }
