362a5b7bb1f520d690675bc5afadd8a9c202a3b7
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-19 17:48:03 -0300

    board/service - board service - board creation

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/board/service/BoardService.java
@@ -0,0 +1,71 @@
+
+// packages
+package com.example.ProjectFlow.modules.board.service;
+
+// imports
+import java.util.List;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+
+// jakarta imports
+import jakarta.transaction.Transactional;
+import jakarta.persistence.NoResultException;
+
+// import repository
+import com.example.ProjectFlow.modules.board.repository.BoardRepository;
+
+// import validator
+import com.example.ProjectFlow.modules.board.validator.BoardValidator;
+
+// import DTOs
+import com.example.ProjectFlow.modules.board.dto.BoardDTO;
+import com.example.ProjectFlow.modules.board.dto.BoardResponseDTO;
+
+// import service
+import com.example.ProjectFlow.modules.project.service.ProjectService;
+
+// import entity
+import com.example.ProjectFlow.modules.board.entity.BoardEntity;
+import com.example.ProjectFlow.modules.project.entity.ProjectEntity;
+
+// import exceptions
+import com.example.ProjectFlow.exception.MultiExceptions;
+
+// import constants
+import com.example.ProjectFlow.common.constants.ResponseMessages;
+
+
+@Service
+public class BoardService {
+ 
+   // properties
+   private final BoardRepository boardRepository;
+   private final BoardValidator boardValidator;
+   private final ProjectService projectService;
+
+
+   // constructor - dependency injection
+   public BoardService(
+      BoardRepository boardRepository,
+      BoardValidator boardValidator,
+      ProjectService projectService
+   ) {
+      this.boardRepository = boardRepository;
+      this.boardValidator = boardValidator;
+      this.projectService = projectService;
+   }
+
+
+   // board creation
+   @Transactional
+   public BoardResponseDTO create(BoardDTO data) {
+      this.boardValidator.projectIdValidate(data.projectId());
+      this.boardValidator.nameValidate(data.name());
+
+      // get project data
+      ProjectEntity project = this.projectService.getEntityById(data.projectId());
+
+      return this.boardRepository.create(data, project);
+   }
+
+}
