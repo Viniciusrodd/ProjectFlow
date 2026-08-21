165bbce7475ef2522d83e793021980186adf2c18
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-21 18:10:03 -0300

    board/controller - board columns controller - create board column

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/board/controller/BoardColumnsController.java
@@ -0,0 +1,68 @@
+
+// packages
+package com.example.ProjectFlow.modules.board.controller;
+
+// imports
+
+// web imports
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+// http imports
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+// swagger imports
+import io.swagger.v3.oas.annotations.Operation;
+
+// import constants
+import com.example.ProjectFlow.common.constants.ApiConstants;
+
+// import services
+import com.example.ProjectFlow.modules.board.service.BoardColumnService;
+
+// import responses
+import com.example.ProjectFlow.common.responses.ApiResponse;
+
+// import DTOs
+import com.example.ProjectFlow.modules.board.dto.BoardColumnsDTO;
+import com.example.ProjectFlow.modules.board.dto.BoardColumnsResponseDTO;
+
+// import constants
+import com.example.ProjectFlow.common.constants.ResponseMessages;
+
+
+@RestController
+@RequestMapping(ApiConstants.BASE_API_PATH)
+public class BoardColumnsController {
+ 
+   // properties
+   private final BoardColumnService boardColumnService;
+
+   // constructor - dependency injection
+   public BoardColumnsController(BoardColumnService boardColumnService) {
+      this.boardColumnService = boardColumnService;
+   }
+
+
+   // create board column
+   @PostMapping("/board/column")
+   @Operation(summary = "Create a board column")
+   public ResponseEntity<ApiResponse<BoardColumnsResponseDTO>> createBoardColumn(
+      @RequestBody BoardColumnsDTO data
+   ) {
+      BoardColumnsResponseDTO boardColumns = this.boardColumnService.create(data);
+
+      ApiResponse<BoardColumnsResponseDTO> response = new ApiResponse.Builder<BoardColumnsResponseDTO>()
+         .success(true)
+         .statusCode(HttpStatus.CREATED.value())
+         .message(ResponseMessages.CREATED)
+         .data(boardColumns)
+         .build();
+
+      return ResponseEntity.status(HttpStatus.CREATED).body(response);
+   }
+
+}
