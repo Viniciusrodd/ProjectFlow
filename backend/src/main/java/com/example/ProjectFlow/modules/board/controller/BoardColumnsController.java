cb16eb5ffa8cc6f1d8db86b365cf83af452fa60c
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-22 17:24:46 -0300

    board/controller - board columns controller - get all board columns

--- backend/src/main/java/com/example/ProjectFlow/modules/board/controller/BoardColumnsController.java
+++ backend/src/main/java/com/example/ProjectFlow/modules/board/controller/BoardColumnsController.java
@@ -3,10 +3,14 @@
 package com.example.ProjectFlow.modules.board.controller;
 
 // imports
+import java.util.List;
+import java.util.UUID;
 
 // web imports
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -63,6 +67,25 @@ public class BoardColumnsController {
          .build();
 
       return ResponseEntity.status(HttpStatus.CREATED).body(response);
+   }
+
+
+   // get all board columns
+   @GetMapping(value = "/board/{boardId}/columns")
+   @Operation(summary = "Get all board columns")
+   public ResponseEntity<ApiResponse<List<BoardColumnsResponseDTO>>> getAllBoardColumns(
+      @PathVariable UUID boardId
+   ) {
+      List<BoardColumnsResponseDTO> boardColumns = this.boardColumnService.getAllColumnsByBoardId(boardId);
+
+      ApiResponse<List<BoardColumnsResponseDTO>> response = new ApiResponse.Builder<List<BoardColumnsResponseDTO>>()
+         .success(true)
+         .statusCode(HttpStatus.OK.value())
+         .message(ResponseMessages.FOUND)
+         .data(boardColumns)
+         .build();
+
+      return ResponseEntity.status(HttpStatus.OK).body(response);
    }
 
 }
