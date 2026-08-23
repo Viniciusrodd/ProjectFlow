fefae6bf0ef53160d1c701879c0006051e25de9b
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-23 16:41:50 -0300

    board/validator - board columns validator - update validations

--- backend/src/main/java/com/example/ProjectFlow/modules/board/validator/BoardColumnsValidator.java
+++ backend/src/main/java/com/example/ProjectFlow/modules/board/validator/BoardColumnsValidator.java
@@ -5,6 +5,7 @@ package com.example.ProjectFlow.modules.board.validator;
 // imports
 import org.springframework.stereotype.Component;
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.UUID;
 
 // import exceptions
@@ -13,7 +14,7 @@ import com.example.ProjectFlow.exception.MultiExceptions;
 // import constants
 import com.example.ProjectFlow.common.constants.ResponseMessages;
 import com.example.ProjectFlow.common.constants.ValidationConstants;
-
+import com.example.ProjectFlow.modules.board.dto.BoardColumnsUpdateDTO;
 // import enum
 import com.example.ProjectFlow.modules.board.enums.BoardEnum;
 
@@ -31,6 +32,7 @@ public class BoardColumnsValidator {
       }
    }
 
+
    // board id validate
    public void boardIdValidate(UUID boardId) {
       if(boardId == null) {
@@ -40,6 +42,7 @@ public class BoardColumnsValidator {
          ));
       }
    }
+
 
    // name validations
    public void nameValidate(String name) {
@@ -67,6 +70,7 @@ public class BoardColumnsValidator {
       }
    }
 
+
    // position validations
    public void positionValidate(Integer position) {
       if(position == null) {
@@ -84,6 +88,7 @@ public class BoardColumnsValidator {
       }
    }
 
+
    // color validations
    public void colorValidate(String color) {
       if(color == null || color.trim().isEmpty()) {
@@ -100,6 +105,64 @@ public class BoardColumnsValidator {
             ResponseMessages.INVALID_DATA
          ));
       }
+   }
+
+
+   // update validations
+   public void updateValidations(BoardColumnsUpdateDTO data) {
+      // name
+      Optional.ofNullable(data.name()).ifPresent(name -> {
+         if(name.trim().isEmpty()) {
+            throw MultiExceptions.badRequest(String.format(
+               "%s: Nome não pode ser vazio",
+               ResponseMessages.BAD_REQUEST
+            ));
+         }
+
+         if(name.length() < ValidationConstants.MIN_BOARD_NAME_LENGTH || name.length() > ValidationConstants.MAX_BOARD_NAME_LENGTH) {
+            throw MultiExceptions.invalid(String.format(
+               "%s: Nome deve estar entre %d e %d caracteres",
+               ValidationConstants.MIN_BOARD_NAME_LENGTH,
+               ValidationConstants.MAX_BOARD_NAME_LENGTH
+            ));
+         }
+
+         if(!BoardEnum.isValid(name)) {
+            throw MultiExceptions.badRequest(String.format(
+               "%s: Nome de quadro inválido. Valores permitidos: %s",
+               ResponseMessages.BAD_REQUEST,
+               Arrays.toString(BoardEnum.values())
+            ));
+         }
+      });
+
+      // position
+      Optional.ofNullable(data.position()).ifPresent(position -> {
+         if(position <= 0) {
+            throw MultiExceptions.invalid(String.format(
+               "%s: Posição deve ser maior que 0",
+               ResponseMessages.INVALID_DATA
+            ));
+         }
+      });
+
+      // color
+      Optional.ofNullable(data.color()).ifPresent(color -> {
+         if(color.trim().isEmpty()) {
+            throw MultiExceptions.badRequest(String.format(
+               "%s: Campo de cor não pode ser vazio",
+               ResponseMessages.BAD_REQUEST
+            ));
+         }
+
+         String hexColor = color.trim();
+         if(!hexColor.matches("^#[0-9A-Fa-f]{6}$")) {
+            throw MultiExceptions.invalid(String.format(
+               "%s: Cores apenas em formato hexadecimal #RRGGBB",
+               ResponseMessages.INVALID_DATA
+            ));
+         }
+      });
    }
 
 }
