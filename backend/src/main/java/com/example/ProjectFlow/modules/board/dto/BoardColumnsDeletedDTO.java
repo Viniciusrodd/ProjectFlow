67c5e320cc3136270b82657d342a7c3fd89dd3f0
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-23 17:40:36 -0300

    board/dto - board columns deleted DTO - setup

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/board/dto/BoardColumnsDeletedDTO.java
@@ -0,0 +1,38 @@
+
+// packages
+package com.example.ProjectFlow.modules.board.dto;
+
+// imports
+import java.util.UUID;
+import java.time.LocalDateTime;
+
+// import enums
+import com.example.ProjectFlow.modules.board.enums.BoardEnum;
+
+// import entity
+import com.example.ProjectFlow.modules.board.entity.BoardColumnsEntity;
+
+
+public record BoardColumnsDeletedDTO (
+
+   UUID id,
+   UUID boardId,
+   BoardEnum name,
+   Integer position,
+   String color,
+   LocalDateTime deletedAt
+
+) {
+
+   public static BoardColumnsDeletedDTO get(BoardColumnsEntity document) {
+      return new BoardColumnsDeletedDTO(
+         document.getId(),
+         document.getBoardId(),
+         document.getName(),
+         document.getPosition(),
+         document.getColor(),
+         document.getDeletedAt()
+      );
+   }
+
+}
