b1cbaee1ca221605e29ad03a901061de8f92432b
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-24 10:57:09 -0300

    task/repository - task creation - setup

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/task/repository/TaskRepository.java
@@ -0,0 +1,58 @@
+
+// packages
+package com.example.ProjectFlow.modules.task.repository;
+
+// imports
+import org.springframework.stereotype.Repository;
+
+// jakarta imports
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import jakarta.persistence.EntityManager;
+
+// import DTOs
+import com.example.ProjectFlow.modules.task.dto.TasksDTO;
+import com.example.ProjectFlow.modules.task.dto.TasksResponseDTO;
+
+// import entity
+import com.example.ProjectFlow.modules.task.entity.TasksEntity;
+import com.example.ProjectFlow.modules.project.entity.ProjectEntity;
+import com.example.ProjectFlow.modules.board.entity.BoardColumnsEntity;
+import com.example.ProjectFlow.modules.user.entity.UserEntity;
+
+// import enums
+import com.example.ProjectFlow.modules.task.enums.PriorityEnum;
+
+
+@Repository
+public class TaskRepository {
+   
+   // properties
+   @PersistenceContext
+   private EntityManager entityManager;
+
+
+   // task creation
+   @Transactional
+   public TasksResponseDTO create(
+      TasksDTO data,
+      ProjectEntity projectEntity,
+      BoardColumnsEntity boardColumnsEntity,
+      UserEntity ownerEntity
+   ) {
+      TasksEntity task = new TasksEntity.Builder()
+         .project(projectEntity)
+         .boardColumn(boardColumnsEntity)
+         .owner(ownerEntity)
+         .title(data.title())
+         .description(data.description())
+         .priority(PriorityEnum.valueOf(data.priority().toUpperCase()))
+         .dueDate(data.dueDate())
+         .build();
+
+      this.entityManager.persist(task);
+
+      return TasksResponseDTO.get(task);
+   }
+
+}
