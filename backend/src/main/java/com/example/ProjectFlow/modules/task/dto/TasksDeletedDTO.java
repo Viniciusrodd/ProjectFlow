31dd118a31aad86fae789e3f85ca10318b0cae3d
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-24 17:08:58 -0300

    task/dto - task deleted DTO - setup

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/task/dto/TasksDeletedDTO.java
@@ -0,0 +1,47 @@
+
+// packages
+package com.example.ProjectFlow.modules.task.dto;
+
+// imports
+import java.util.UUID;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+// import enums
+import com.example.ProjectFlow.modules.task.enums.PriorityEnum;
+
+// import entity
+import com.example.ProjectFlow.modules.task.entity.TasksEntity;
+
+
+public record TasksDeletedDTO (
+
+   UUID id,
+   UUID projectId,
+   UUID columnId,
+   UUID ownerId,
+   String title,
+   String description,
+   PriorityEnum priority,
+   LocalDate due_date,
+   LocalDateTime completedAt,
+   LocalDateTime deletedAt
+
+) {
+   
+   public static TasksDeletedDTO get(TasksEntity document) {
+      return new TasksDeletedDTO(
+         document.getId(),
+         document.getProjectId(),
+         document.getBoardColumnId(),
+         document.getOwnerId(),
+         document.getTitle(),
+         document.getDescription(),
+         document.getPriority(),
+         document.getDueDate(),
+         document.getCompletedAt(),
+         document.getDeletedAt()         
+      );
+   }
+
+}
