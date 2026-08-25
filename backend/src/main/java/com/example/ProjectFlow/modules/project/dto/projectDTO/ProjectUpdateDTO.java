fb7f702b60095f5dfe7a4c80c727a357467ef9f5
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-25 20:03:54 -0300

    project/dto - update project dto - changes

--- backend/src/main/java/com/example/ProjectFlow/modules/project/dto/projectDTO/ProjectUpdateDTO.java
+++ backend/src/main/java/com/example/ProjectFlow/modules/project/dto/projectDTO/ProjectUpdateDTO.java
@@ -6,6 +6,7 @@ package com.example.ProjectFlow.modules.project.dto.projectDTO;
 public record ProjectUpdateDTO (
  
    String name,
-   String description
+   String description,
+   String status
 
 ) {}
