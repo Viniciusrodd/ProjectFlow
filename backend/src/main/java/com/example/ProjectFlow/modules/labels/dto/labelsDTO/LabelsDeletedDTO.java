3dfce33e7f0e073c2b8c6f349007e2b635b42f69
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-29 17:10:58 -0300

    labels/dto - deleted label dto - setup

--- backend/src/main/java/com/example/ProjectFlow/modules/labels/dto/labelsDTO/LabelsDeletedDTO.java
+++ backend/src/main/java/com/example/ProjectFlow/modules/labels/dto/labelsDTO/LabelsDeletedDTO.java
@@ -3,28 +3,31 @@
 package com.example.ProjectFlow.modules.labels.dto.labelsDTO;
 
 // imports
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 // import entity
 import com.example.ProjectFlow.modules.labels.entity.LabelsEntity;
 
 
-public record LabelsResponseDTO (
+public record LabelsDeletedDTO (
 
    UUID id,
    UUID projectId,
    String name,
-   String color
+   String color,
+   LocalDateTime deletedAt
 
 ) {
-
-   public static LabelsResponseDTO get(LabelsEntity document) {
-      return new LabelsResponseDTO(
+ 
+   public static LabelsDeletedDTO get(LabelsEntity document) {
+      return new LabelsDeletedDTO(
          document.getId(),
          document.getProject().getId(),
          document.getName(),
-         document.getColor()
+         document.getColor(),
+         document.getDeletedAt()
       );
    }
-
+   
 }
