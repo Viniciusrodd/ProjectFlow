67ca515dd8d4440324e399707d448e1f1b8f4dc0
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-08-31 19:54:36 -0300

    task/repository - task labels - get all labels by task

--- backend/src/main/java/com/example/ProjectFlow/modules/task/repository/TaskLabelsRepository.java
+++ backend/src/main/java/com/example/ProjectFlow/modules/task/repository/TaskLabelsRepository.java
@@ -3,6 +3,9 @@
 package com.example.ProjectFlow.modules.task.repository;
 
 // imports
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.stereotype.Repository;
 
 // jakarta imports
@@ -12,6 +15,7 @@ import jakarta.persistence.EntityManager;
 
 // import DTOs
 import com.example.ProjectFlow.modules.task.dto.taskLabelsDTO.TaskLabelsResponseDTO;
+import com.example.ProjectFlow.modules.task.dto.taskLabelsDTO.labelsByTaskResponseDTO;
 
 // import entity
 import com.example.ProjectFlow.modules.task.entity.TaskLabelsEntity;
@@ -38,6 +42,29 @@ public class TaskLabelsRepository {
       this.entityManager.persist(taskLabel);
 
       return TaskLabelsResponseDTO.get(taskLabel);
+   }
+
+
+   // get all labels by task id
+   public List<labelsByTaskResponseDTO> getAllByTaskId(UUID taskId) {
+      List<TaskLabelsEntity> taskLabelsDocument = this.entityManager
+         .createQuery(
+            "SELECT tl FROM TaskLabelsEntity tl " +
+            "JOIN FETCH tl.label " +
+            "WHERE tl.task.id = :taskId " +
+            "ORDER BY tl.createdAt ASC",
+            TaskLabelsEntity.class
+         )
+         .setParameter("taskId", taskId)
+         .getResultList();
+         
+      List<labelsByTaskResponseDTO> taskLabels = new ArrayList<>();
+
+      for(TaskLabelsEntity taskLabel : taskLabelsDocument) {
+         taskLabels.add(labelsByTaskResponseDTO.get(taskLabel));
+      }
+
+      return taskLabels;
    }
 
 }
