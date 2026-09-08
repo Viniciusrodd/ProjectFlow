af0c2f9a884d9ec8c4bfa29ca19b5575ea568753
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-09-08 18:40:17 -0300

    task/dto - task checklist update dto - setup

---
+++ backend/src/main/java/com/example/ProjectFlow/modules/task/dto/taskChecklistDTO/TaskChecklistUpdateDTO.java
@@ -0,0 +1,11 @@
+
+// packages
+package com.example.ProjectFlow.modules.task.dto.taskChecklistDTO;
+
+
+public record TaskChecklistUpdateDTO (
+ 
+   String description,
+   Integer position
+
+) {}
