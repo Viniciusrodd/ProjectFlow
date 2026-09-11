6c94f457299717c165e8322cb0dec0f980c8b6ad
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2026-09-11 17:01:16 -0300

    task/service - task labels - returned datas refactoring

--- backend/src/main/java/com/example/ProjectFlow/modules/task/service/TaskLabelService.java
+++ backend/src/main/java/com/example/ProjectFlow/modules/task/service/TaskLabelService.java
@@ -4,6 +4,7 @@ package com.example.ProjectFlow.modules.task.service;
 
 // imports
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -37,6 +38,9 @@ import com.example.ProjectFlow.exception.MultiExceptions;
 // import constants
 import com.example.ProjectFlow.common.constants.ResponseMessages;
 
+// import mapper
+import com.example.ProjectFlow.modules.task.mapper.TaskLabelsMapper;
+
 
 @Service
 public class TaskLabelService {
@@ -46,18 +50,22 @@ public class TaskLabelService {
    private final TaskLabelsValidator taskLabelsValidator;
    private final TaskService taskService;
    private final LabelService labelService;
+   private final TaskLabelsMapper taskLabelsMapper;
+
 
    // constructor - dependency injection
    public TaskLabelService(
       TaskLabelsRepository taskLabelsRepository,
       TaskLabelsValidator taskLabelsValidator,
       TaskService taskService,
-      LabelService labelService
+      LabelService labelService,
+      TaskLabelsMapper taskLabelsMapper
    ) {
       this.taskLabelsRepository = taskLabelsRepository;
       this.taskLabelsValidator = taskLabelsValidator;
       this.taskService = taskService;
       this.labelService = labelService;
+      this.taskLabelsMapper = taskLabelsMapper;
    }
 
 
@@ -76,7 +84,10 @@ public class TaskLabelService {
       // task and label project - validation
       this.taskLabelsValidator.projectValidation(task.getProjectId(), label.getProjectId());
 
-      return this.taskLabelsRepository.create(task, label);
+      // creation
+      TaskLabelsEntity taskLabelsEntity = this.taskLabelsRepository.create(task, label);
+
+      return this.taskLabelsMapper.toTaskLabelsResponseDTO(taskLabelsEntity);
    }
 
 
@@ -87,13 +98,19 @@ public class TaskLabelService {
       // task existence - check
       this.taskService.existsById(taskId);
 
-      List<LabelsByTaskResponseDTO> labels = this.taskLabelsRepository.getAllByTaskId(taskId);
+      List<TaskLabelsEntity> taskLabelsEntity = this.taskLabelsRepository.getAllByTaskId(taskId);
 
-      if(labels.isEmpty()) {
+      if(taskLabelsEntity.isEmpty()) {
          throw MultiExceptions.notFound(String.format(
             "%s: Etiquetas de tarefa não existem",
             ResponseMessages.NOT_FOUND
          ));
+      }
+
+      // mapping
+      List<LabelsByTaskResponseDTO> labels = new ArrayList<>();
+      for(TaskLabelsEntity label : taskLabelsEntity) {
+         labels.add(this.taskLabelsMapper.toLabelsByTaskResponseDTO(label));
       }
 
       return labels;
@@ -107,13 +124,19 @@ public class TaskLabelService {
       // label existence - check
       this.labelService.existsById(labelId);
 
-      List<TasksByLabelResponseDTO> tasks = this.taskLabelsRepository.getAllByLabelId(labelId);
+      List<TaskLabelsEntity> taskLabelsEntity = this.taskLabelsRepository.getAllByLabelId(labelId);
 
-      if(tasks.isEmpty()) {
+      if(taskLabelsEntity.isEmpty()) {
          throw MultiExceptions.notFound(String.format(
             "%s: Tarefas da etiqueta não existem",
             ResponseMessages.NOT_FOUND
          ));
+      }
+
+      // mapping
+      List<TasksByLabelResponseDTO> tasks = new ArrayList<>();
+      for(TaskLabelsEntity task : taskLabelsEntity) {
+         tasks.add(this.taskLabelsMapper.toTasksByLabelResponseDTO(task));
       }
 
       return tasks;
@@ -175,7 +198,9 @@ public class TaskLabelService {
       this.taskLabelsValidator.idValidate(id);
 
       try {
-         return this.taskLabelsRepository.removeRelation(id);
+         TaskLabelsEntity taskLabelsEntity = this.taskLabelsRepository.removeRelation(id);
+
+         return this.taskLabelsMapper.toTaskLabelsDeletedDTO(taskLabelsEntity);
       }
       catch (NoResultException error) {
          throw MultiExceptions.notFound(String.format(
